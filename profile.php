f74891607de6bc20e49cd5d0f9d3123a4bb5abce
wajahatabbasi86 <37658509+wajahatabbasi86@users.noreply.github.com>
2018-03-22 16:21:54 +0500

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,61 +1,35 @@
 <!DOCTYPE html>
 <html>
-<head>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  max-width: 300px;
-  margin: auto;
-  text-align: center;
-  font-family: arial;
-}
-
-.title {
-  color: grey;
-  font-size: 18px;
-}
-
-button {
-  border: none;
-  outline: 0;
-  display: inline-block;
-  padding: 8px;
-  color: white;
-  background-color: #000;
-  text-align: center;
-  cursor: pointer;
-  width: 100%;
-  font-size: 18px;
-}
-
-a {
-  text-decoration: none;
-  font-size: 22px;
-  color: black;
-}
-
-button:hover, a:hover {
-  opacity: 0.7;
-}
-</style>
-</head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <body>
 
-<h2 style="text-align:center">User Profile Card</h2>
+<div class="w3-container w3-green">
+  <h1>Cloud Computiong</h1> 
+  <p>Following are the Service Models in Cloud Computing</p> 
+</div>
 
-<div class="card">
-  <img src="/w3images/team2.jpg" alt="John" style="width:100%">
-  <h1>John Doe</h1>
-  <p class="title">CEO & Founder, Example</p>
-  <p>Harvard University</p>
-  <div style="margin: 24px 0;">
-    <a href="#"><i class="fa fa-dribbble"></i></a> 
-    <a href="#"><i class="fa fa-twitter"></i></a>  
-    <a href="#"><i class="fa fa-linkedin"></i></a>  
-    <a href="#"><i class="fa fa-facebook"></i></a> 
- </div>
- <p><button>Contact</button></p>
+<div class="w3-row-padding">
+  <div class="w3-third">
+    <h2>Infrastructure-as-a-Service (IaaS)</h2>
+    <p align="justify">IaaS means you're buying access to raw computing hardware over the Net, such as servers or storage. Since you buy what you need and pay-as-you-go, this is often referred to as utility computing. Ordinary web hosting is a simple example of IaaS: you pay a monthly subscription or a per-megabyte/gigabyte fee to have a hosting company serve up files for your website from their servers.</p>
+  </div>
+  
+  <div class="w3-third">
+    <h2>Platform-as-a-Service (PaaS) </h2>
+    <p align="justify">PaaS means you develop applications using Web-based tools so they run on systems software and hardware provided by another company. So, for example, you might develop your own ecommerce website but have the whole thing, including the shopping cart, checkout, and payment mechanism running on a merchant's server. App Cloud (from salesforce.com) and the Google App Engine are examples of PaaS.</p> 
+  </div>
+  
+  <div class="w3-third">
+    <h2>Software-as-a-Service (SaaS )</h2>
+    <p align="justify">SaaS is a software deployment model that delivers software applications as online, on-demand services. Software vendors increasingly deliver their software applications as online services, in accordance with the above-mentioned SaaS model, and thus become SaaS providers. This enables their customers to use these software services on demand.</p>
+  </div>
+  
+</div>
+
+<div>
+<h2>Advantages</h2>
+<p align="justify">The pros of cloud computing are obvious and compelling. If your business is selling books or repairing shoes, why get involved in the nitty gritty of buying and maintaining a complex computer system? If you run an insurance office, do you really want your sales agents wasting time running anti-virus software, upgrading word-processors, or worrying about hard-drive crashes? Do you really want them cluttering your expensive computers with their personal emails, illegally shared MP3 files, and naughty YouTube videos—when you could leave that responsibility to someone else? Cloud computing allows you to buy in only the services you want, when you want them, cutting the upfront capital costs of computers and peripherals. You avoid equipment going out of date and other familiar IT problems like ensuring system security and reliability. You can add extra services (or take them away) at a moment's notice as your business needs change. It's really quick and easy to add new applications or services to your business without waiting weeks or months for the new computer (and its software) to arrive.</p>
 </div>
 
 </body>
